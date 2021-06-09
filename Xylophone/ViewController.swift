e8366ba06347fa5dcd8a67980f7ba8448da804db
Vladimir Khrolovich <v.khroch@gmail.com>
2021-06-09 13:32:13 +0300

    complete 7section

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
@@ -14,7 +15,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
     }
 
+    var player: AVAudioPlayer!
+    
+    func playSound(button: String) {
+            let url = Bundle.main.url(forResource: button, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
 
+    
+    @IBAction func tapC(_ sender: UIButton) {
+        playSound(button: sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+              sender.alpha = 1.0
+          }
+    }
+    
     
 
 }
